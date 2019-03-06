a2faab95d8fa7b4fcf01a27656f54564a3979718
dsnaumov <dsnaumovgit@mail.ru>
2019-03-06 23:48:40 +0300

    Четвертое задание

--- JSON_to_HTML.py
+++ JSON_to_HTML.py
@@ -8,26 +8,55 @@ class JsonToHtml:
         self.file = file or 'source.json'
         self.row = ""
 
+    # load json file
     def _load_json(self):
         f = open(self.file)
         self.data = json.load(f)
         f.close()
 
-    def _parse(self, arg=None):
-        arg = arg or self.data
-        if isinstance(arg, list):
-            return "<ul>{}</ul>".format("".join(["<li>{}</li>".format(self._parse(x)) for x in self.data]))
+    # parse list by element
+    def _parse_list(self, arg):
+        return "<ul>{}</ul>".format("".join(["<li>{}</li>".format(self._parse(x)) for x in arg]))
+
+    # parse dict by element
+    def _parse_dict(self, arg):
         result = ""
         for key, value in arg.items():
-            result = "{result}<{tag}>{value}</{tag}>".format(result=result, tag=key, value=value)
+            result = "{result}<{tag}>{value}</{tag}>".format(result=result, tag=key, value=self._parse(value))
         return result
 
+    def _parse(self, arg=None):
+        """
+        The parse function converts a file in a json file into an html file.
+        As an optional parameter takes a string in a json format (default parameter: self.data).
+        Returns a string in html format.
+            >>> self._parse('[{"TAG": "description"}]')
+            return: "<ul><li><TAG>description</TAG></li></ul>"
+        """
+
+        arg = arg or self.data
+        if isinstance(arg, list):
+            return self._parse_list(arg)
+
+        elif isinstance(arg, dict):
+            return self._parse_dict(arg)
+
+        else:
+            return arg
+
+    # save to html file
     def _save_html(self):
         f = open("index.html", "w")
         f.write(self.row)
         f.close()
 
     def convert(self):
+        """
+        Function - user interface.
+        Convert source.json file to index.html file
+            >>> obj.convert()
+        """
+
         self._load_json()
         self.row = self._parse()
         self._save_html()
--- Unittest.py
+++ Unittest.py
@@ -5,23 +5,21 @@ from JSON_to_HTML import JsonToHtml
 class TestJsonToHtml(unittest.TestCase):
 
     def test_parse(self):
-        jth.data = [
-            {"title": "some text", "body": "description"},
+        jth.data = [[
+            {"title": {"some text": "cosmo"}},
+            {"spans": "Title",
+            "content":[
+                        {"p": "tIT", "span": "44LE", "title": "tITLE", "body": "text676"}
+                    ]},
+            {"body": "description"},
             {"title": "3456", "body": "text676"},
             {"title": "tITLE", "body": "TEXT"}
-        ]
+        ]]
         self.assertEqual(jth._parse(),
-                         "<ul><li><title>some text</title><body>description</body></li><li><title>3456</title>"
-                         "<body>text676</body></li><li><title>tITLE</title><body>TEXT</body></li></ul>")
-
-    def test_non_parse(self):
-        jth.data = [
-            {"title": "some text"},
-            {"TEXT"}
-        ]
-        with self.assertRaises(AttributeError):
-            jth._parse()
-
+                         "<ul><li><ul><li><title><some text>cosmo</some text></title></li><li><spans>Title</spans>"
+                         "<content><ul><li><p>tIT</p><span>44LE</span><title>tITLE</title><body>text676</body></li>"
+                         "</ul></content></li><li><body>description</body></li><li><title>3456</title><body>"
+                         "text676</body></li><li><title>tITLE</title><body>TEXT</body></li></ul></li></ul>")
 
 if __name__ == "__main__":
     jth = JsonToHtml()
